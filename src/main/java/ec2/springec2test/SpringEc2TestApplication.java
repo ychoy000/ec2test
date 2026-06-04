e23c55c25d340c1dd93d7197c6ec73d85298f51c
ychoy <ychoy000@gmail.com>
2026-06-04 11:34:55 +0900

    first commit

---
+++ src/main/java/ec2/springec2test/SpringEc2TestApplication.java
@@ -0,0 +1,13 @@
+package ec2.springec2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEc2TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEc2TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec2/springec2test/SpringEc2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package ec2.springec2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEc2TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
